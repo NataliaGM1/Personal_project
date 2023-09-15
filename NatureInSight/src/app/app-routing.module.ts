2eaf2d2a06aa47a6fa26c86cd946511e2cb5070c
NataliaGM1 <ngmihalache@gmail.com>
2023-09-15 09:46:37 +0200

    hamburger menu right

--- NatureInSight/src/app/app-routing.module.ts
+++ NatureInSight/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PlantListComponent } from './pages/plant-list/plant-list.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
   {path: 'plant-list', component: PlantListComponent},
   { path: '**', component:PageNotFoundComponent}
